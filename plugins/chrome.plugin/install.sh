5abfb46d09c5763e14055e85f518b8cf030199bf
richardson-souza <richardson.allan.souza@gmail.com>
2016-08-20 02:07:47 -0400

    update

--- plugins/chrome.plugin/install.sh
+++ plugins/chrome.plugin/install.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-rpm --import https://dl-ssl.google.com/linux/linux_signing_key.pub
-dnf -y install https://dl.google.com/linux/direct/google-chrome-stable_current_$(uname -i).rpm
-
-
-# Fix for double icon in dock
-file="/usr/share/applications/google-chrome.desktop"
-str_arr=("Desktop Entry" "NewWindow Shortcut Group" "NewIncognito Shortcut Group")
-fix_str="StartupWMClass=Google-chrome-stable"
-
-for i in "${str_arr[@]}"
-do
-    line=`grep -n "\\[$i\\]" $file | cut -d : -f 1`
-    ((line++))
-    sed -i "$line"'i\'"$fix_str" $file
-done
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+apt install -f -y
