1e158eadcc0ca45b9db814c289628b471acc3c30
richardson-souza <richardson.allan.souza@gmail.com>
2016-08-20 16:52:18 -0400

    update

--- plugins/arctheme.plugin/install.sh
+++ plugins/arctheme.plugin/install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Installing arc-theme ..."
+{
 sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
 cd /tmp
 wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
 apt-key add - < Release.key
-apt update && apt install -y arc-theme
+apt update
+apt install -y arc-theme
+} &> /dev/null
+echo "done!"
--- plugins/arctheme.plugin/uninstall.sh
+++ plugins/arctheme.plugin/uninstall.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "Remove arch-theme ..."
+{
 apt remove -y arc-theme
 sudo rm -r /etc/apt/sources.list.d/arc-theme.list
 apt-key del BEB6D886
 apt update
+} &> /dev/null
+echo "done!"
