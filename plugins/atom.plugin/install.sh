3afc3d70db3e3f910e023cc1f34a48b0f9b2f293
richardson-souza <richardson.allan.souza@gmail.com>
2016-08-20 22:20:14 -0400

    update atom.plugin

--- plugins/atom.plugin/install.sh
+++ plugins/atom.plugin/install.sh
@@ -3,9 +3,8 @@
 echo "Installing Atom..."
 {
 cd /tmp
-apt install -qq -y git gconf2 gconf-service libgtk2.0-0 libudev0 libudev1 libgcrypt11 libgcrypt20 libnotify4 libxtst6 libnss3 python gvfs-bin xdg-utils libcap2
-wget -q https://atom-installer.github.com/v1.9.9/atom-amd64.deb?s=1471476867&ext=.deb
-dpkg -i atom-amd64.deb
+wget -q https://atom.io/download/deb -O atom.deb
+dpkg -i atom.deb
 apt install -qq -f -y -o=Dpkg::Use-Pty=0apt
 } > /dev/null
 echo "done!"
